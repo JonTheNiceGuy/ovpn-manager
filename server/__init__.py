be3da63b8d8ffeb87289bba5700cbaf93edaecb4
Jon Spriggs <jon@sprig.gs>
2025-06-30 12:27:06 +0100

    Add 500-error handling

--- server/__init__.py
+++ server/__init__.py
@@ -63,12 +63,16 @@ def create_app():
     app.register_blueprint(tasks_bp, url_prefix='/tasks')
 
     # --- Register Custom Error Handlers ---
-    @app.errorhandler(404)
-    def page_not_found(e):
-        return render_template('404.html'), 404
-
     @app.errorhandler(403)
     def forbidden(e):
         return render_template('403.html'), 403
+    
+    @app.errorhandler(404)
+    def page_not_found(e):
+        return render_template('404.html'), 404
+    
+    @app.errorhandler(500)
+    def internal_server_error(e):
+        return render_template('500.html'), 500
 
     return app
