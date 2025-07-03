d6e9904156cc3d2d8d1741833e6ca0b9ad52a69e
Jon Spriggs <jon@sprig.gs>
2025-07-03 17:12:59 +0100

    Add more logging from gunicorn

--- server/logging.py
+++ server/logging.py
@@ -1,3 +1,4 @@
+import logging
 from gunicorn.glogging import Logger
 
 class CustomGunicornLogger(Logger):
@@ -5,6 +6,24 @@ class CustomGunicornLogger(Logger):
     A custom Gunicorn logger that filters out Kubernetes health check probes
     from the access logs.
     """
+    def setup(self, cfg):
+        """
+        This method is called by Gunicorn at startup to configure logging.
+        """
+        super().setup(cfg)
+
+        # --- THIS IS THE FIX ---
+        # Get the Flask application's logger
+        app_logger = logging.getLogger('flask.app')
+
+        # Add all of Gunicorn's handlers to the Flask logger.
+        # This makes Flask's debug messages go to the same place as Gunicorn's logs.
+        for handler in self.error_log.handlers:
+            app_logger.addHandler(handler)
+        
+        # Finally, ensure the Flask logger is set to DEBUG level.
+        app_logger.setLevel(logging.DEBUG)
+
     def access(self, resp, req, environ, request_time):
         """
         This method is called by Gunicorn to log an access request.
